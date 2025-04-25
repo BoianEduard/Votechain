4d5fb0cee9ad48c8812208cf192f4556e4429f64
BEduard <ediboian3@gmail.com>
2025-04-25 23:38:16 +0300

    Candidate picture API implementet in Election Page + minor changes

--- server/app.mjs
+++ server/app.mjs
@@ -3,24 +3,27 @@ import cors from 'cors'
 import cookieParser from 'cookie-parser';
 import authRoutes from './routes/authRoutes.mjs'
 import electionRoutes from './routes/electionRoutes.mjs'
+import path from 'path';
 
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
 const app = express()
-
 const corsOptions = {
   origin: 'http://localhost:3000',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
   optionsSuccessStatus: 200
-}
+};
 
 app.use(cors(corsOptions))
 app.use(express.json({ limit: '10mb' }))
 app.use(express.urlencoded({ extended: true, limit: '10mb' }))
+app.use('/uploads/candidates', express.static(path.join(__dirname, 'public', 'uploads', 'candidates')));
+app.use('/candidates', express.static(path.join(__dirname, 'public', 'uploads', 'candidates')));
 app.use(cookieParser())
+console.log(path.join(__dirname, 'public', 'uploads'));
 
 app.use("/api/auth", authRoutes)
 app.use("/api/election", electionRoutes)
-app.use("/api/candidates",authRoutes)
 
 export default app
