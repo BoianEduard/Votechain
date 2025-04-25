ac1515a72ec09d925f90e13719adbbae0cb31e8a
BEduard <ediboian3@gmail.com>
2025-04-25 23:43:27 +0300

    Fixed API call for Election Page

--- client/src/pages/ElectionPage/ElectionPage.js
+++ client/src/pages/ElectionPage/ElectionPage.js
@@ -2,7 +2,7 @@ import {useEffect, useState} from 'react';
 import { AlertCircle, Search, Filter } from 'lucide-react';
 import { useDispatch, useSelector } from 'react-redux';
 import ElectionCard from '../../components/ElectionView/ElectionCard';
-import {fetchElectionDetails} from "../../redux/thunks/electionThunks";
+import {fetchAllElections} from "../../redux/thunks/electionThunks";
 
 const ElectionPage = () => {
   const dispatch = useDispatch();
@@ -10,7 +10,7 @@ const ElectionPage = () => {
   const { elections, loading, error } = useSelector(state => state.election);
 
   useEffect(() => {
-    dispatch(fetchElectionDetails());
+    dispatch(fetchAllElections());
   },[])
 
   return (
